a9ac14e44cc14c9bb8c8e8ebf2522efc925ab75b
AkaPetrovic <akapetrovic@gmail.com>
2023-09-17 15:17:31 +0200

    Ispravljena responzivnost na home stranici

--- src/app/(webstore)/cart/page.jsx
+++ src/app/(webstore)/cart/page.jsx
@@ -13,42 +13,44 @@ function Cart() {
   return (
     <main className={styles.cart}>
       {itemsInCart.length > 0 ? (
-        <div className={styles.items}>
-          {itemsInCart.map((item, index) => (
-            <div
-              className={
-                index === itemsInCart.length - 1
-                  ? `${styles.itemInCart} ${styles.lastItemInCart}`
-                  : styles.itemInCart
-              }
-              key={item.gbooksID}
-            >
-              <h1 className={styles.title}>{item.name}</h1>
-              <div className={styles.amount}>
-                <button
-                  className={styles.amountControls}
-                  onClick={() => decreaseAmountByOne(item.gbooksID)}
-                >
-                  <FontAwesomeIcon icon={faMinus} />
-                </button>
-                <div>{item.amountInCart}</div>
-                <button
-                  className={styles.amountControls}
-                  onClick={() => increaseAmountByOne(item.gbooksID)}
-                >
-                  <FontAwesomeIcon icon={faPlus} />
-                </button>
+        <>
+          <div className={styles.items}>
+            {itemsInCart.map((item, index) => (
+              <div
+                className={
+                  index === itemsInCart.length - 1
+                    ? `${styles.itemInCart} ${styles.lastItemInCart}`
+                    : styles.itemInCart
+                }
+                key={item.gbooksID}
+              >
+                <h1 className={styles.title}>{item.name}</h1>
+                <div className={styles.amount}>
+                  <button
+                    className={styles.amountControls}
+                    onClick={() => decreaseAmountByOne(item.gbooksID)}
+                  >
+                    <FontAwesomeIcon icon={faMinus} />
+                  </button>
+                  <div>{item.amountInCart}</div>
+                  <button
+                    className={styles.amountControls}
+                    onClick={() => increaseAmountByOne(item.gbooksID)}
+                  >
+                    <FontAwesomeIcon icon={faPlus} />
+                  </button>
+                </div>
+                <p className={styles.totalPrice}>
+                  total: RSD {item.price * item.amountInCart}
+                </p>
               </div>
-              <p className={styles.totalPrice}>
-                total: RSD {item.price * item.amountInCart}
-              </p>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+          <button className={styles.confirmButton}>Confirm purchase</button>
+        </>
       ) : (
         <h1 className={styles.emptyCartMessage}>Cart is currently empty...</h1>
       )}
-      <button className={styles.confirmButton}>Confirm purchase</button>
     </main>
   );
 }
